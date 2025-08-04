bcf61d84dcb2f585775b40a26d5c55ced653394a
prametanaset <prametanaset147@gmail.com>
2025-08-04 14:35:03 +0700

    Remove unused components and implement new UI elements for invoice and project creation pages

---
+++ app/components/base/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <Card  :class="cn('rounded-lg shadow-none', props.class)"
+>
+    <slot />
+  </Card>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@/lib/utils';
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+
+
+</script>
--- app/pages/test/index.vue
+++ app/pages/test/index.vue
@@ -1,17 +1,52 @@
 <template>
-  <div class="flex flex-1 flex-col gap-4 p-4 h-[calc(100vh-theme(spacing.20))]">
-    <div class="min-h-[100vh] flex flex-1 rounded-xl bg-muted/50 md:min-h-min items-center justify-center">
-      <div class="flex flex-col items-center justify-center">
-        <FeatureInvoiceFirstCreate />
-      </div>
-    </div>
-  </div>
+  <BaseCard  class="w-[350px]">
+      <CardHeader>
+      <CardTitle>Create project</CardTitle>
+      <CardDescription>Deploy your new project in one-click.</CardDescription>
+    </CardHeader>
+    <CardContent>
+      <form>
+        <div class="grid items-center w-full gap-4">
+          <div class="flex flex-col space-y-1.5">
+            <Label for="name">Name</Label>
+            <Input id="name" placeholder="Name of your project" />
+          </div>
+          <div class="flex flex-col space-y-1.5">
+            <Label for="framework">Framework</Label>
+            <Select>
+              <SelectTrigger id="framework">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="nuxt">
+                  Nuxt
+                </SelectItem>
+                <SelectItem value="next">
+                  Next.js
+                </SelectItem>
+                <SelectItem value="sveltekit">
+                  SvelteKit
+                </SelectItem>
+                <SelectItem value="astro">
+                  Astro
+                </SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </form>
+    </CardContent>
+    <CardFooter class="flex justify-between px-6 pb-6">
+      <Button variant="outline">
+        Cancel
+      </Button>
+      <Button>Deploy</Button>
+    </CardFooter>
+  </BaseCard>
 </template>
 
-<script lang="ts" setup>
-const name = ref('');
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isLoading = ref(false)
 </script>
-
-<style>
-
-</style>

5f170e05e106bc235f9ef2e74e7d5aec67b38375
prametanaset <prametanaset147@gmail.com>
2025-08-08 13:50:34 +0700

    feat: update InvoicePaper and default layout for improved structure and responsiveness

--- app/components/feature/invoice/InvoicePaper.vue
+++ app/components/feature/invoice/InvoicePaper.vue
@@ -124,10 +124,6 @@
     </table>
   </div>
 
-  <!--  Footer  -->
-  <div class="border-t-2 pt-4 text-xs text-gray-500 text-center mt-16">
-    Please pay the invoice before the due date. You can pay the invoice by logging in to your account from our client portal.
-  </div>
 
 </div>
 </template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <SidebarProvider>
+  <SidebarProvider v-model:open="open">
     <LayoutAppSidebar />
     <SidebarInset>
       <AppSidebarHeader>
@@ -18,8 +18,34 @@
 </template>
 
 <script lang="ts" setup>
+import { useMediaQuery } from '@vueuse/core';
 import AppSidebarHeader from '~/components/layout/AppSidebarHeader.vue';
+const isStuck = ref(false);
+const sentinel = ref<HTMLElement | null>(null);
+const open = ref(true);
+const isLg = useMediaQuery("(min-width: 1457px)");
 
+onMounted(() => {
+  const observer = new IntersectionObserver(
+    ([e]) => {
+      if (e) {
+        isStuck.value = !e.isIntersecting;
+      }
+    },
+    { threshold: [1] }
+  );
+  if (sentinel.value) {
+    observer.observe(sentinel.value);
+  }
+
+  // ตั้งค่าครั้งแรกตามขนาดจริงบน client
+  open.value = isLg.value;
+
+  // ฟังการเปลี่ยนแปลงครั้งต่อ ๆ ไป
+  watch(isLg, (val) => {
+    open.value = val;
+  });
+});
 
 </script>
 

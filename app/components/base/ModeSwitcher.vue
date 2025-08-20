64c2848eee20f35ac625ba894bf0968cad313e3e
prametanaset <prametanaset147@gmail.com>
2025-08-20 20:39:37 +0700

    create mode switcher

---
+++ app/components/base/ModeSwitcher.vue
@@ -0,0 +1,16 @@
+<template>
+  <Tabs v-model="colorMode" class="w-[400px]">
+    <TabsList>
+      <TabsTrigger value="light"><SunIcon /> </TabsTrigger>
+      <TabsTrigger value="dark"><MoonIcon /></TabsTrigger>
+      <TabsTrigger value="system"><Monitor /></TabsTrigger>
+    </TabsList>
+  </Tabs>
+</template>
+
+<script setup lang="ts">
+import { useColorMode } from "@vueuse/core";
+import { SunIcon, MoonIcon, Monitor } from "lucide-vue-next";
+
+const colorMode = useColorMode(); // Ref<'light' | 'dark' | 'auto'>
+</script>
--- app/pages/test/index.vue
+++ app/pages/test/index.vue
@@ -1,21 +1,16 @@
-<template>
-  <div class="relative h-60 w-full flex flex-col items-center justify-center gap-4">
-    <div class="flex flex-row items-center justify-center gap-4">
-      <span>Enable Effect</span>
-      <Switch v-model="enabled" />
-    </div>
-    <span
-      v-if="enabled"
-      class="opacity-50 text-4xl font-semibold"
-    >
-      Hover anywhere
-    </span>
-    <FluidCursor v-if="enabled" />
-  </div>
-</template>
+<script setup lang="ts">
+import { useColorMode } from "@vueuse/core";
+import { SunIcon, MoonIcon, Monitor } from "lucide-vue-next";
 
-<script lang="ts" setup>
-import FluidCursor from '~/components/ui/fluid-cursor/FluidCursor.vue';
-
-const enabled = ref(true);
+const colorMode = useColorMode(); // Ref<'light' | 'dark' | 'auto'>
 </script>
+
+<template>
+  <Tabs v-model="colorMode" class="w-[400px]">
+    <TabsList>
+      <TabsTrigger value="light"><SunIcon /> </TabsTrigger>
+      <TabsTrigger value="dark"><MoonIcon /></TabsTrigger>
+      <TabsTrigger value="system"><Monitor /></TabsTrigger>
+    </TabsList>
+  </Tabs>
+</template>
